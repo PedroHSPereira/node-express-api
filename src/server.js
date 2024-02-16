5e537bee701f1faf123c3bbfa891341ad96dda24
PedroHSPereira <pedro.pereira2@aluno.ifsp.edu.br>
2024-02-15 22:00:06 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import {PORT, HOST} from './config.js'
+import {users} from './db-memory/user.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({message: "Bem-vindo a API!"})
+})
+
+app.get('/user', (req, res) => {
+  res.json({
+    success: "Usuários listados com sucesso!",
+    users
+  })
+})
+
+app.post('/user', (req, res) => {
+  const user = req.body
+  user.id = users[users.length - 1].id + 1 
+  users.push(user)
+  res.json({
+    success: "Usuários listados com sucesso!",
+    users
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em ${HOST}:${PORT}`)
+})
